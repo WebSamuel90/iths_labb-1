1ce84e2becf2b290bcb561af08a457a4e62c60fd
samue <samue@SAMUEL>
2026-09-16 14:00:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿string stringToCheck = "29535123p48723487597645723645";
+var correctNumbers = new List<string>();
+long sum = 0;
+
+for  (int i = 0; i < stringToCheck.Length; i++)
+{
+    if (!char.IsDigit(stringToCheck[i]))
+    {
+        continue;
+    }
+  
+    char start = stringToCheck[i];
+    int startPos = i;
+
+    for (int j = startPos + 1; j < stringToCheck.Length; j++)
+    {
+
+        if (!char.IsDigit(stringToCheck[j]))
+        {
+            break;
+        }
+          
+        if (start == stringToCheck[j])
+        {
+            int endPos = j;
+            string substring = stringToCheck.Substring(startPos, endPos - startPos + 1);
+            correctNumbers.Add(substring);
+
+            PrintSubstringWithColor(stringToCheck, startPos, endPos);
+
+            Console.WriteLine();
+
+            break;
+        }
+    }
+    Console.ResetColor();
+}
+
+foreach (var number in correctNumbers)
+{
+    sum += long.Parse(number);
+}
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Sum: {sum}");
+Console.ResetColor();
+
+static void PrintSubstringWithColor(string text, int startIndex, int endIndex)
+{
+    if (startIndex < 0 || endIndex >= text.Length || startIndex > endIndex)
+    {
+        return;
+    }
+
+    Console.Write(text.Substring(0, startIndex));
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write(text.Substring(startIndex, endIndex - startIndex + 1));
+    Console.ResetColor();
+    Console.Write(text.Substring(endIndex + 1));
+}
